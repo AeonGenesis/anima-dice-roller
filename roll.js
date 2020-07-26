3e9b43b99af4b96feb13110690547b06e827d181
AeonGenesis <exveras@hotmail.com>
2020-07-26 03:34:33 -0400

    Add files via upload

--- roll.js
+++ roll.js
@@ -1,6 +1,6 @@
 const attributes = {
     str: 0,
-    strModifer: 0,
+    strModifier: 0,
     dex: 0,
     dexModifier: 0,
     int: 0,
@@ -22,7 +22,6 @@ const ranges = [
 const getModifier = (stat, ranges) => {
     console.log(stat, ranges);
     for (const range of ranges) {
-        console.log(range);
         if (stat >= range.min && stat <= range.max) {
             return range.modifier;
         }
@@ -32,38 +31,17 @@ const getModifier = (stat, ranges) => {
 
 attributes.str = parseInt(prompt("What is your STR?"), 10);
 attributes.strModifier = getModifier(attributes.str, ranges)
-
-// attributes.dex = prompt("What is your DEX?");
-// attributes.int = prompt("What is your INT?");
-
-// if(attributes.str === 6 || attributes.str === 7) {
-//     attributes.strModifer = 5;
-// } else if (attributes.str === 8 || attributes.str === 9) {
-//     attributes.strModifer = 10;
-// } else if (attributes.str === 10) {
-//     attributes.strModifier = 15; 
-// } else if (attributes.str === 11 || attributes.str === 12) {
-//         attributes.strModifier = 20;
-// } else if (attributes.str === 13 || attributes.str === 14) {
-//     attributes.strModifier = 25
-// } else if (attributes.str === 15 || attributes.str === 16) {
-//     attributes.strModfier = 30;
-// } else if (attributes.str === 16 || attributes.str === 17) {
-//     attributes.strModifier = 35;
-// } else if (attributes.str === 18 || attributes.str === 19) {
-//     attributes.strModifer = 40;
-// } else if (attributes.str === 20) {
-//     attributes.strModifer = 45;
-// };
+attributes.dex = parseInt(prompt("What is your DEX?"), 10);
+attributes.dexModifier = getModifier(attributes.dex, ranges)
+attributes.int = parseInt(prompt("What is your INT?"), 10);
+attributes.intModifier = getModifier(attributes.int, ranges)
 
 
-attributes.str = parseInt(prompt("What is your STR?"), 10);
 
 const d100 = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-d100(1, 100);
 
 
 
